3fb2e9c191f09ae47d9ee3dc2a210402c6745747
Pham-Nguyen-Khoa <khoapnse183214@fpt.edu.vn>
2024-10-09 21:58:51 +0700

    Tạo model chat

---
+++ models/chat.model.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database"); // Database
+// const { UPDATE } = require("sequelize/lib/query-types");
+const Chat = sequelize.define('Chat', {
+    ChatID: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        primaryKey: true
+    },
+    User_ID: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+    },
+    Content: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    Images: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    }
+}, {
+    tableName: 'chat',
+    timestamps: false
+});
+
+module.exports = Chat;
