668a5959f89cf64f8f9a33abf43cbb4eeac3350e
Pham-Nguyen-Khoa <khoapnse183214@fpt.edu.vn>
2024-10-10 23:30:58 +0700

    lưu code lần 2

--- public/js/style.js
+++ public/js/style.js
@@ -617,6 +617,7 @@ if (buttonSubmitAppointment) {
     }
   });
 // }
+}
 
 
 let MY_BANK = {
@@ -734,7 +735,9 @@ async function checkPaid(priceService, generateRandomText, intervalId) {
 
 const closeModalButton = document.getElementById("closeModalMessage");
 if (closeModalButton) {
+
   closeModalButton.addEventListener("click", function () {
+    console.log("Neronmen")
     document.getElementById("confirmationModal").style.display = "none";
   });
 } else {
@@ -747,30 +750,30 @@ if (closeModalButton) {
 // const marquee = document.querySelector('.marquee');
 // let index = 0;
 
-// function scrollText() {
-//   const lines = marquee.querySelectorAll('p');
-//   lines.forEach((line, i) => {
-//     line.style.display = i === index ? 'block' : 'none';
-//   });
-//   index = (index + 1) % lines.length;
-// }
+function scrollText() {
+  const lines = marquee.querySelectorAll('p');
+  lines.forEach((line, i) => {
+    line.style.display = i === index ? 'block' : 'none';
+  });
+  index = (index + 1) % lines.length;
+}
 
-// setInterval(scrollText, 5000); 
+setInterval(scrollText, 5000); 
 
-// console.log("Neronmen")
-// function initializeSlider() {
-//   const slide = document.querySelector('.review-slide');
-//   const groups = document.querySelectorAll('.review-group');
-//   if(slide && groups){
-//       let currentIndex = 0;
-//       function nextSlide() {
-//         currentIndex = (currentIndex + 1) % groups.length;
-//         slide.style.transform = `translateX(-${currentIndex * 100}%)`;
-//     }
+console.log("Neronmen")
+function initializeSlider() {
+  const slide = document.querySelector('.review-slide');
+  const groups = document.querySelectorAll('.review-group');
+  if(slide && groups){
+      let currentIndex = 0;
+      function nextSlide() {
+        currentIndex = (currentIndex + 1) % groups.length;
+        slide.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
   
-//     setInterval(nextSlide,4000);
+    setInterval(nextSlide,4000);
   
-//   }
-// }
+  }
+}
 
-// document.addEventListener('DOMContentLoaded', initializeSlider)
+document.addEventListener('DOMContentLoaded', initializeSlider);
