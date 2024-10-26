f3b9d9b3b1240d70225f91cdd4eb7d05982aa08d
Pham-Nguyen-Khoa <khoapnse183214@fpt.edu.vn>
2024-10-26 14:11:41 +0700

    Update otp forgot password become professional

--- controller/client/forgot_password.controller.js
+++ controller/client/forgot_password.controller.js
@@ -3,6 +3,7 @@ const Account1 = require("../../models/account1.model");
 const sendMail = require("../../helpers/nodemailer");
 const generate = require("../../helpers/generate");
 const Sequelize = require("../../config/database");
+const { Op } = require("sequelize");
 const Forgot_Password = require("../../models/register.model");
 const db = require("../../config/database");
 const md5 = require("md5");
@@ -33,14 +34,32 @@ module.exports.forgotPost = async (req, res) => {
 // [GET] localhost:/auth/password/otp
 module.exports.otp = async (req, res) => {
   const email = req.query.email;
+  const existingOtp = await Forgot_Password.findOne({
+    where: {
+      email: email,
+      created_at: {
+        [Op.gt]: new Date(Date.now() - 3 * 60 * 1000) // Check if OTP is within the last 3 minutes
+      }
+    }
+  });
+  if (!existingOtp) {
   const otp = generate.generateRandomNumber(6);
   const query = `Insert into forgot_password(email,otp) VALUES ('${email}','${otp}')`;
   await Sequelize.query(query);
+  const messageMail = `<div style="font-family: Arial, sans-serif; text-align: center; padding: 20px;">
+      <h2 style="color: #4CAF50;">Xác minh tài khoản</h2>
+      <p>Chào bạn,</p>
+      <p>Mã OTP của bạn là: <b style="font-size: 24px; color: #FF5733;">${otp}</b></p>
+      <p>Mã này tồn tại trong 3 phút.</p>
+      <p>Vui lòng không chia sẻ mã này với bất kỳ ai.</p>
+      <p>Trân trọng,<br>Đội ngũ hỗ trợ</p>
+    </div>`;
   sendMail.sendMail(
     email,
-    "Mã OTP xác minh lấy lại mật khẩu",
-    `Mã OTP của bạn là: <b>${otp}</b>. Mã này tồn tại trong 3 phút`
-  );
+    "Mã OTP xác minh lấy lại mật khẩu", 
+    messageMail
+    );
+  }
 
   res.render("client/pages/forgot-password/otp.pug", {
     pageTitle: "Trang OTP",
@@ -63,12 +82,29 @@ module.exports.otpPost = async (req, res) => {
     req.flash("error", "Mã OTP không đúng!");
     res.redirect("back");
     return;
-  }
-  // const user = await Account.findOne({
-  //   where: {
-  //     email: email,
-  //   },
-  // });
+  } 
+  const existingOtp = await Forgot_Password.findOne({
+    where: {
+      email: email,
+      otp: otp,
+      created_at: {
+        [Op.lt]: new Date(Date.now() - 3 * 60 * 1000) 
+      }
+    },
+    order: [['created_at', 'DESC']], 
+    limit: 1 
+  });
+  
+  if (existingOtp) {
+    req.flash("error", "Mã OTP đã hết hạn!");
+    const email = req.body.email;
+    const resetOtp = true;
+    return res.render("client/pages/forgot-password/otp.pug", {
+      email: email,
+      resetOtp: resetOtp,
+    });
+  } 
+  
   const user = await Account1.findOne({
     where: {
       Email: email,
